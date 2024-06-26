bda0aa4e406a464df03255b2741f9af7724990d5
Shanu <shanu@Shanus-MacBook-Pro.local>
2024-06-26 17:25:41 +0530

    url

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('register/', views.register, name='register'),
+    #path('register/', views.register, name='register'),
     path('success/<int:registration_number>/', views.success, name='success'),
     path('userdisplay/', views.userdisplay, name='register_and_display'),
     #path('generate-pdf/<int:registration_number>/', views.generate_pdf, name='generate_pdf'),
