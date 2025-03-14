719b001338aacc2f9073c49e1dcf70b53eac2af5
Shanu Munambath <neo@Shanz-MacBook-M3.local>
2025-03-14 12:23:28 +0530

    changes on 14-03-2025

--- core/urls.py
+++ core/urls.py
@@ -7,10 +7,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    #path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('success/<int:registration_number>/', views.success, name='success'),
     path('userdisplay/', views.userdisplay, name='register_and_display'),
-    #path('generate-pdf/<int:registration_number>/', views.generate_pdf, name='generate_pdf'),
+    path('generate-pdf/<int:registration_number>/', views.generate_pdf, name='generate_pdf'),
     path('hall_ticket_pdf/<int:registration_number>/',views.hall_ticket_pdf, name='hall_ticket_pdf'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
